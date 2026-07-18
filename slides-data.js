f3525e1edbe26785e0997b2fbcad3f57cf235cb6
leehee4343 <leehee4343@users.noreply.github.com>
2026-07-18 23:11:23 +0900

    deploy: 2026-07-18 슬라이드 데이터 배포

--- slides-data.js
+++ slides-data.js
@@ -1,13 +1,657 @@
 // 자동 생성 파일 — 서버 없이 index.html을 직접 열었을 때 표시되는 슬라이드 목록입니다. 직접 수정하지 마세요.
 // 이미지를 추가·순서변경하려면 '프로그램 시작.bat'으로 편집 서버를 켠 뒤 브라우저에서 사용하세요.
 window.STATIC_PROJECT_DATA = {
-  "currentProjectId": "default",
+  "currentProjectId": "mrqeuxndln1ucp",
   "projects": [
     {
-      "id": "default",
-      "name": "기본 프로젝트",
-      "slides": [],
-      "public": false
+      "id": "mrqeuxndln1ucp",
+      "name": "백투더스쿨 사업계획서 요약본(CahtGPT)",
+      "slides": [
+        {
+          "id": "mrqfge6r525og2",
+          "name": "Slide#01",
+          "file": "images/mrqeuxndln1ucp/mrqfge6r525og2.png",
+          "order": 0
+        },
+        {
+          "id": "mrqfge7zqucc6h",
+          "name": "Slide#02",
+          "file": "images/mrqeuxndln1ucp/mrqfge7zqucc6h.png",
+          "order": 1
+        },
+        {
+          "id": "mrqfge99iidroz",
+          "name": "Slide#03",
+          "file": "images/mrqeuxndln1ucp/mrqfge99iidroz.png",
+          "order": 2
+        },
+        {
+          "id": "mrqfgealpj1ppd",
+          "name": "Slide#04",
+          "file": "images/mrqeuxndln1ucp/mrqfgealpj1ppd.png",
+          "order": 3
+        },
+        {
+          "id": "mrqfgebx8w9gce",
+          "name": "Slide#05",
+          "file": "images/mrqeuxndln1ucp/mrqfgebx8w9gce.png",
+          "order": 4
+        },
+        {
+          "id": "mrqfgecxgqm1uq",
+          "name": "Slide#06",
+          "file": "images/mrqeuxndln1ucp/mrqfgecxgqm1uq.png",
+          "order": 5
+        },
+        {
+          "id": "mrqfgedluec9xw",
+          "name": "Slide#07",
+          "file": "images/mrqeuxndln1ucp/mrqfgedluec9xw.png",
+          "order": 6
+        },
+        {
+          "id": "mrqfgeeecxqye0",
+          "name": "Slide#08",
+          "file": "images/mrqeuxndln1ucp/mrqfgeeecxqye0.png",
+          "order": 7
+        },
+        {
+          "id": "mrqfgef7gtm7mz",
+          "name": "Slide#09",
+          "file": "images/mrqeuxndln1ucp/mrqfgef7gtm7mz.png",
+          "order": 8
+        },
+        {
+          "id": "mrqfgefv1mx8re",
+          "name": "Slide#10",
+          "file": "images/mrqeuxndln1ucp/mrqfgefv1mx8re.png",
+          "order": 9
+        },
+        {
+          "id": "mrqfgegobcs4d1",
+          "name": "Slide#11",
+          "file": "images/mrqeuxndln1ucp/mrqfgegobcs4d1.png",
+          "order": 10
+        },
+        {
+          "id": "mrqfgeheiwqsfp",
+          "name": "Slide#12",
+          "file": "images/mrqeuxndln1ucp/mrqfgeheiwqsfp.png",
+          "order": 11
+        },
+        {
+          "id": "mrqfgei6ge3evv",
+          "name": "Slide#13",
+          "file": "images/mrqeuxndln1ucp/mrqfgei6ge3evv.png",
+          "order": 12
+        },
+        {
+          "id": "mrqfgeiyjji787",
+          "name": "Slide#14",
+          "file": "images/mrqeuxndln1ucp/mrqfgeiyjji787.png",
+          "order": 13
+        },
+        {
+          "id": "mrqfgejlfz96ku",
+          "name": "Slide#15",
+          "file": "images/mrqeuxndln1ucp/mrqfgejlfz96ku.png",
+          "order": 14
+        },
+        {
+          "id": "mrqfgekaommuoo",
+          "name": "Slide#16",
+          "file": "images/mrqeuxndln1ucp/mrqfgekaommuoo.png",
+          "order": 15
+        }
+      ],
+      "public": true
+    },
+    {
+      "id": "mrqfrxbyk82lwo",
+      "name": "백투더스쿨 사업계획서(Full)",
+      "slides": [
+        {
+          "id": "mrqfsazeu6kn2f",
+          "name": "슬라이드#1",
+          "file": "images/mrqfrxbyk82lwo/mrqfsazeu6kn2f.png",
+          "order": 0
+        },
+        {
+          "id": "mrqfsb01lnndsd",
+          "name": "슬라이드#2",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb01lnndsd.png",
+          "order": 1
+        },
+        {
+          "id": "mrqfsb0sydwl0w",
+          "name": "슬라이드#3",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb0sydwl0w.png",
+          "order": 2
+        },
+        {
+          "id": "mrqfsb1e1ot3k8",
+          "name": "슬라이드#4",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb1e1ot3k8.png",
+          "order": 3
+        },
+        {
+          "id": "mrqfsb1zq6dpcu",
+          "name": "슬라이드#5",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb1zq6dpcu.png",
+          "order": 4
+        },
+        {
+          "id": "mrqfsb2mxlxro4",
+          "name": "슬라이드#6",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb2mxlxro4.png",
+          "order": 5
+        },
+        {
+          "id": "mrqfsb3eu3eh54",
+          "name": "슬라이드#7",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb3eu3eh54.png",
+          "order": 6
+        },
+        {
+          "id": "mrqfsb479ze417",
+          "name": "슬라이드#8",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb479ze417.png",
+          "order": 7
+        },
+        {
+          "id": "mrqfsb4ujt6aw1",
+          "name": "슬라이드#9",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb4ujt6aw1.png",
+          "order": 8
+        },
+        {
+          "id": "mrqfsb5jx2d4td",
+          "name": "슬라이드#10",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb5jx2d4td.png",
+          "order": 9
+        },
+        {
+          "id": "mrqfsb68h8q0cm",
+          "name": "슬라이드#11",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb68h8q0cm.png",
+          "order": 10
+        },
+        {
+          "id": "mrqfsb70teo0xn",
+          "name": "슬라이드#12",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb70teo0xn.png",
+          "order": 11
+        },
+        {
+          "id": "mrqfsb7oe3aram",
+          "name": "슬라이드#13",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb7oe3aram.png",
+          "order": 12
+        },
+        {
+          "id": "mrqfsb8hsbpdff",
+          "name": "슬라이드#14",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb8hsbpdff.png",
+          "order": 13
+        },
+        {
+          "id": "mrqfsb9bw3o3j5",
+          "name": "슬라이드#15",
+          "file": "images/mrqfrxbyk82lwo/mrqfsb9bw3o3j5.png",
+          "order": 14
+        },
+        {
+          "id": "mrqfsba4x5egl1",
+          "name": "슬라이드#16",
+          "file": "images/mrqfrxbyk82lwo/mrqfsba4x5egl1.png",
+          "order": 15
+        },
+        {
+          "id": "mrqfsbax30zjqb",
+          "name": "슬라이드#17",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbax30zjqb.png",
+          "order": 16
+        },
+        {
+          "id": "mrqfsbbneedcfp",
+          "name": "슬라이드#18",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbbneedcfp.png",
+          "order": 17
+        },
+        {
+          "id": "mrqfsbcbjigjed",
+          "name": "슬라이드#19",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbcbjigjed.png",
+          "order": 18
+        },
+        {
+          "id": "mrqfsbd2yb0vw7",
+          "name": "슬라이드#20",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbd2yb0vw7.png",
+          "order": 19
+        },
+        {
+          "id": "mrqfsbdpy0tw9y",
+          "name": "슬라이드#21",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbdpy0tw9y.png",
+          "order": 20
+        },
+        {
+          "id": "mrqfsbehy3rsuv",
+          "name": "슬라이드#22",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbehy3rsuv.png",
+          "order": 21
+        },
+        {
+          "id": "mrqfsbf5f0n4cr",
+          "name": "슬라이드#23",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbf5f0n4cr.png",
+          "order": 22
+        },
+        {
+          "id": "mrqfsbfvjh9uaa",
+          "name": "슬라이드#24",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbfvjh9uaa.png",
+          "order": 23
+        },
+        {
+          "id": "mrqfsbgnhsl7w3",
+          "name": "슬라이드#25",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbgnhsl7w3.png",
+          "order": 24
+        },
+        {
+          "id": "mrqfsbhe259pqs",
+          "name": "슬라이드#26",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbhe259pqs.png",
+          "order": 25
+        },
+        {
+          "id": "mrqfsbi4qudyrb",
+          "name": "슬라이드#27",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbi4qudyrb.png",
+          "order": 26
+        },
+        {
+          "id": "mrqfsbitlyygee",
+          "name": "슬라이드#28",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbitlyygee.png",
+          "order": 27
+        },
+        {
+          "id": "mrqfsbjlmj117e",
+          "name": "슬라이드#29",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbjlmj117e.png",
+          "order": 28
+        },
+        {
+          "id": "mrqfsbkd87rgxh",
+          "name": "슬라이드#30",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbkd87rgxh.png",
+          "order": 29
+        },
+        {
+          "id": "mrqfsbl2dpsu76",
+          "name": "슬라이드#31",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbl2dpsu76.png",
+          "order": 30
+        },
+        {
+          "id": "mrqfsblrpzbbcp",
+          "name": "슬라이드#32",
+          "file": "images/mrqfrxbyk82lwo/mrqfsblrpzbbcp.png",
+          "order": 31
+        },
+        {
+          "id": "mrqfsbmes9luoe",
+          "name": "슬라이드#33",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbmes9luoe.png",
+          "order": 32
+        },
+        {
+          "id": "mrqfsbn4ygleik",
+          "name": "슬라이드#34",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbn4ygleik.png",
+          "order": 33
+        },
+        {
+          "id": "mrqfsbnsm8v9ku",
+          "name": "슬라이드#35",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbnsm8v9ku.png",
+          "order": 34
+        },
+        {
+          "id": "mrqfsbouz5f8f3",
+          "name": "슬라이드#36",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbouz5f8f3.png",
+          "order": 35
+        },
+        {
+          "id": "mrqfsbprrvswev",
+          "name": "슬라이드#37",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbprrvswev.png",
+          "order": 36
+        },
+        {
+          "id": "mrqfsbqnw9yz22",
+          "name": "슬라이드#38",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbqnw9yz22.png",
+          "order": 37
+        },
+        {
+          "id": "mrqfsbrgnsq87o",
+          "name": "슬라이드#39",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbrgnsq87o.png",
+          "order": 38
+        },
+        {
+          "id": "mrqfsbs7wltscm",
+          "name": "슬라이드#40",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbs7wltscm.png",
+          "order": 39
+        },
+        {
+          "id": "mrqfsbsxwuxx9r",
+          "name": "슬라이드#41",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbsxwuxx9r.png",
+          "order": 40
+        },
+        {
+          "id": "mrqfsbtprgo0x8",
+          "name": "슬라이드#42",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbtprgo0x8.png",
+          "order": 41
+        },
+        {
+          "id": "mrqfsbukdfuzrj",
+          "name": "슬라이드#43",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbukdfuzrj.png",
+          "order": 42
+        },
+        {
+          "id": "mrqfsbvfto19mz",
+          "name": "슬라이드#44",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbvfto19mz.png",
+          "order": 43
+        },
+        {
+          "id": "mrqfsbw7pie8hl",
+          "name": "슬라이드#45",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbw7pie8hl.png",
+          "order": 44
+        },
+        {
+          "id": "mrqfsbwy458uga",
+          "name": "슬라이드#46",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbwy458uga.png",
+          "order": 45
+        },
+        {
+          "id": "mrqfsbxnjz080r",
+          "name": "슬라이드#47",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbxnjz080r.png",
+          "order": 46
+        },
+        {
+          "id": "mrqfsbyhegdu71",
+          "name": "슬라이드#48",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbyhegdu71.png",
+          "order": 47
+        },
+        {
+          "id": "mrqfsbzbx7jn7v",
+          "name": "슬라이드#49",
+          "file": "images/mrqfrxbyk82lwo/mrqfsbzbx7jn7v.png",
+          "order": 48
+        },
+        {
+          "id": "mrqfsc04pxx7s3",
+          "name": "슬라이드#50",
+          "file": "images/mrqfrxbyk82lwo/mrqfsc04pxx7s3.png",
+          "order": 49
+        },
+        {
+          "id": "mrqfsc0ws9mgt9",
+          "name": "슬라이드#51",
+          "file": "images/mrqfrxbyk82lwo/mrqfsc0ws9mgt9.png",
+          "order": 50
+        },
+        {
+          "id": "mrqfsc1qzph94q",
+          "name": "슬라이드#52",
+          "file": "images/mrqfrxbyk82lwo/mrqfsc1qzph94q.png",
+          "order": 51
+        },
+        {
+          "id": "mrqfsc2kb0qgex",
+          "name": "슬라이드#53",
+          "file": "images/mrqfrxbyk82lwo/mrqfsc2kb0qgex.png",
+          "order": 52
+        },
+        {
+          "id": "mrqfsc3ewbnq7y",
+          "name": "슬라이드#54",
+          "file": "images/mrqfrxbyk82lwo/mrqfsc3ewbnq7y.png",
+          "order": 53
+        },
+        {
+          "id": "mrqfsc43zficb7",
+          "name": "슬라이드#55",
+          "file": "images/mrqfrxbyk82lwo/mrqfsc43zficb7.png",
+          "order": 54
+        },
+        {
+          "id": "mrqfsc4vz65q5q",
+          "name": "슬라이드#56",
+          "file": "images/mrqfrxbyk82lwo/mrqfsc4vz65q5q.png",
+          "order": 55
+        },
+        {
+          "id": "mrqfsc5ngo3y39",
+          "name": "슬라이드#57",
+          "file": "images/mrqfrxbyk82lwo/mrqfsc5ngo3y39.png",
+          "order": 56
+        },
+        {
+          "id": "mrqfsc6e3q0c03",
+          "name": "슬라이드#58",
+          "file": "images/mrqfrxbyk82lwo/mrqfsc6e3q0c03.png",
+          "order": 57
+        },
+        {
+          "id": "mrqfsc75ppl0it",
+          "name": "슬라이드#59",
+          "file": "images/mrqfrxbyk82lwo/mrqfsc75ppl0it.png",
+          "order": 58
+        },
+        {
+          "id": "mrqfsc7vjcbm9h",
+          "name": "슬라이드#60",
+          "file": "images/mrqfrxbyk82lwo/mrqfsc7vjcbm9h.png",
+          "order": 59
+        },
+        {
+          "id": "mrqfsc8qf5mt18",
+          "name": "슬라이드#61",
+          "file": "images/mrqfrxbyk82lwo/mrqfsc8qf5mt18.png",
+          "order": 60
+        },
+        {
+          "id": "mrqfsc9ietgeh4",
+          "name": "슬라이드#62",
+          "file": "images/mrqfrxbyk82lwo/mrqfsc9ietgeh4.png",
+          "order": 61
+        },
+        {
+          "id": "mrqfsca9wbjl5z",
+          "name": "슬라이드#63",
+          "file": "images/mrqfrxbyk82lwo/mrqfsca9wbjl5z.png",
+          "order": 62
+        },
+        {
+          "id": "mrqfscaz1quokn",
+          "name": "슬라이드#64",
+          "file": "images/mrqfrxbyk82lwo/mrqfscaz1quokn.png",
+          "order": 63
+        },
+        {
+          "id": "mrqfscbq581a0l",
+          "name": "슬라이드#65",
+          "file": "images/mrqfrxbyk82lwo/mrqfscbq581a0l.png",
+          "order": 64
+        },
+        {
+          "id": "mrqfsccf0ysh3b",
+          "name": "슬라이드#66",
+          "file": "images/mrqfrxbyk82lwo/mrqfsccf0ysh3b.png",
+          "order": 65
+        },
+        {
+          "id": "mrqfscdee1bghk",
+          "name": "슬라이드#67",
+          "file": "images/mrqfrxbyk82lwo/mrqfscdee1bghk.png",
+          "order": 66
+        },
+        {
+          "id": "mrqfsce81u61su",
+          "name": "슬라이드#68",
+          "file": "images/mrqfrxbyk82lwo/mrqfsce81u61su.png",
+          "order": 67
+        },
+        {
+          "id": "mrqfscf17vi7h5",
+          "name": "슬라이드#69",
+          "file": "images/mrqfrxbyk82lwo/mrqfscf17vi7h5.png",
+          "order": 68
+        },
+        {
+          "id": "mrqfscfv15jt6l",
+          "name": "슬라이드#70",
+          "file": "images/mrqfrxbyk82lwo/mrqfscfv15jt6l.png",
+          "order": 69
+        },
+        {
+          "id": "mrqfscgoy9ak3c",
+          "name": "슬라이드#71",
+          "file": "images/mrqfrxbyk82lwo/mrqfscgoy9ak3c.png",
+          "order": 70
+        },
+        {
+          "id": "mrqfschegeighb",
+          "name": "슬라이드#72",
+          "file": "images/mrqfrxbyk82lwo/mrqfschegeighb.png",
+          "order": 71
+        },
+        {
+          "id": "mrqfsci3x8gdry",
+          "name": "슬라이드#73",
+          "file": "images/mrqfrxbyk82lwo/mrqfsci3x8gdry.png",
+          "order": 72
+        },
+        {
+          "id": "mrqfscitvmg5lh",
+          "name": "슬라이드#74",
+          "file": "images/mrqfrxbyk82lwo/mrqfscitvmg5lh.png",
+          "order": 73
+        },
+        {
+          "id": "mrqfscjloqr2d4",
+          "name": "슬라이드#75",
+          "file": "images/mrqfrxbyk82lwo/mrqfscjloqr2d4.png",
+          "order": 74
+        },
+        {
+          "id": "mrqfsckbv6kq7r",
+          "name": "슬라이드#76",
+          "file": "images/mrqfrxbyk82lwo/mrqfsckbv6kq7r.png",
+          "order": 75
+        },
+        {
+          "id": "mrqfscl6a43s2b",
+          "name": "슬라이드#77",
+          "file": "images/mrqfrxbyk82lwo/mrqfscl6a43s2b.png",
+          "order": 76
+        },
+        {
+          "id": "mrqfsclx28viqg",
+          "name": "슬라이드#78",
+          "file": "images/mrqfrxbyk82lwo/mrqfsclx28viqg.png",
+          "order": 77
+        },
+        {
+          "id": "mrqfscmupwsa2e",
+          "name": "슬라이드#79",
+          "file": "images/mrqfrxbyk82lwo/mrqfscmupwsa2e.png",
+          "order": 78
+        },
+        {
+          "id": "mrqfscnkzheq31",
+          "name": "슬라이드#80",
+          "file": "images/mrqfrxbyk82lwo/mrqfscnkzheq31.png",
+          "order": 79
+        },
+        {
+          "id": "mrqfscob241a79",
+          "name": "슬라이드#81",
+          "file": "images/mrqfrxbyk82lwo/mrqfscob241a79.png",
+          "order": 80
+        },
+        {
+          "id": "mrqfscp59i4kmu",
+          "name": "슬라이드#82",
+          "file": "images/mrqfrxbyk82lwo/mrqfscp59i4kmu.png",
+          "order": 81
+        },
+        {
+          "id": "mrqfscq0o7h4u4",
+          "name": "슬라이드#83",
+          "file": "images/mrqfrxbyk82lwo/mrqfscq0o7h4u4.png",
+          "order": 82
+        },
+        {
+          "id": "mrqfscr47af1k0",
+          "name": "슬라이드#84",
+          "file": "images/mrqfrxbyk82lwo/mrqfscr47af1k0.png",
+          "order": 83
+        },
+        {
+          "id": "mrqfscse3xkygh",
+          "name": "슬라이드#85",
+          "file": "images/mrqfrxbyk82lwo/mrqfscse3xkygh.png",
+          "order": 84
+        },
+        {
+          "id": "mrqfsctom2n6p0",
+          "name": "슬라이드#86",
+          "file": "images/mrqfrxbyk82lwo/mrqfsctom2n6p0.png",
+          "order": 85
+        },
+        {
+          "id": "mrqfscujop7pf5",
+          "name": "슬라이드#87",
+          "file": "images/mrqfrxbyk82lwo/mrqfscujop7pf5.png",
+          "order": 86
+        },
+        {
+          "id": "mrqfscvb1o5ns0",
+          "name": "슬라이드#88",
+          "file": "images/mrqfrxbyk82lwo/mrqfscvb1o5ns0.png",
+          "order": 87
+        },
+        {
+          "id": "mrqfscw3acquu7",
+          "name": "슬라이드#89",
+          "file": "images/mrqfrxbyk82lwo/mrqfscw3acquu7.png",
+          "order": 88
+        },
+        {
+          "id": "mrqfscwu23bt63",
+          "name": "슬라이드#90",
+          "file": "images/mrqfrxbyk82lwo/mrqfscwu23bt63.png",
+          "order": 89
+        }
+      ],
+      "public": true
     }
   ]
 };
